b1b035bfc32b1bb5e965da8f932322288bb1c426
Achim Herwig <achim.herwig@wodca.de>
2011-03-29 20:44:06 +0200

    Added member example.

---
+++ 04-ClassMembers/member.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <sstream>
+
+class SomeClass
+{
+public:
+    SomeClass(std::string n) 
+		: name(n), mNumber(0.0) {}
+
+    std::string name;
+
+	double getNumber() const
+		{ return mNumber; }
+	void setNumber(double n)
+		{ 
+			if (n>3.141592654)
+				n = -1;
+			mNumber = n; 
+		}
+
+private:
+	double mNumber;
+};
+
+
+#include <boost/python.hpp>
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(member)
+{
+    class_<SomeClass>("SomeClass", init<std::string>())
+		.def_readwrite("name", & SomeClass::name)
+		.add_property("number", &SomeClass::getNumber, &SomeClass::setNumber)
+    ;
+
+}
+
+
---
+++ 04-ClassMembers/member.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import member
+
+m1 = member.SomeClass("Pavel")
+print "name =",m1.name
+
+m1.number = 7.3
+print "number =",m1.number
+
