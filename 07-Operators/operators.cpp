f7ccc9a4df169f2746883b7f9b733ddacf5b7fa6
Achim Herwig <achim.herwig@wodca.de>
2011-04-02 09:48:42 +0200

    Added repr operator.

--- 07-Operators/operators.cpp
+++ 07-Operators/operators.cpp
@@ -7,15 +7,16 @@ public:
 	NumberLike(int n = 0) : _n(n) {}
 	NumberLike& operator+= (int i)
 		{ _n += i; return *this; }
-	std::string toString() const
-		{ std::stringstream s; s<<_n; return s.str(); }
+	std::string str() const
+		{ std::stringstream s; s << _n; return s.str(); }
+    std::string repr() const		
+		{ std::stringstream s; s << "NumberLike("<< _n<<")"; return s.str(); }
 private:
 	int _n;
 };
 
 NumberLike operator+(NumberLike n, int i)
 	{ n += i; return n; };
-
 
 #include <boost/python.hpp>
 using namespace boost::python;
@@ -26,6 +27,7 @@ BOOST_PYTHON_MODULE(operators)
 	class_<NumberLike>("NumberLike")
 		.def(init< optional<int> >())
 		.def(self + int())
-		.def("__str__", &NumberLike::toString)
+		.def("__str__", &NumberLike::str)
+		.def("__repr__", &NumberLike::repr)
 	;
 }	
