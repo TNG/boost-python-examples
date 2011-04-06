23665e82be07d6362056f569cc759cd346c3a525
Achim Herwig <achim.herwig@wodca.de>
2011-04-06 23:41:57 +0200

    Exception example

---
+++ 14-Exceptions/myexceptions.cpp
@@ -0,0 +1,21 @@
+#include "Python.h"
+
+struct OutOfSteakException {};
+
+void translateException( const OutOfSteakException& x) {
+	PyErr_SetString(PyExc_UserWarning, "The meat is gone, go for the cheese....");
+};
+
+void someFunction() {
+	throw OutOfSteakException();
+};
+
+#include "boost/python.hpp"
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(myexceptions)
+{
+	register_exception_translator<OutOfSteakException>(translateException);
+
+	def("someFunction", someFunction);
+}
---
+++ 14-Exceptions/myexceptions.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from myexceptions import someFunction
+
+try:
+	someFunction()
+except UserWarning as ex:
+	print "Exception %s caught: \"%s\""%(type(ex),ex)
+
