f6a17c3e9999d5c8676cf980bb989886b66f9935
Achim Herwig <achim.herwig@wodca.de>
2011-04-06 23:16:45 +0200

    simple iterator example

---
+++ 13-Iterators/iterators.cpp
@@ -0,0 +1,36 @@
+#include <sstream>
+#include <string>
+#include <vector>
+
+class Example {
+public:
+	Example() 
+	{}
+	void add(const std::string& s)
+	{
+		mS.push_back(s);
+	}
+
+	std::vector<std::string>::iterator begin() 
+	{
+		return mS.begin();
+	}
+	std::vector<std::string>::iterator end()
+	{
+		return mS.end();
+	}
+
+private:
+	std::vector<std::string> mS;
+};
+
+#include <boost/python.hpp>
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(iterators)
+{
+	class_<Example>("Example")
+		.def("strings", range(&Example::begin, &Example::end))
+		.def("add", &Example::add)
+	;
+}	
---
+++ 13-Iterators/iterators.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from sys import stdout
+from iterators import Example
+
+e = Example()
+
+e.add("a")
+e.add("b")
+e.add("c")
+
+print [ s for s in e.strings() ]
+
