8faa1e912dd113f8fa443524c115dc4e4defa7b6
Achim Herwig <achim.herwig@tngtech.com>
2017-09-08 22:13:14 +0200

    Accept list argument.
    Fixes #16

--- 02-ExposingClasses/classes.cpp
+++ 02-ExposingClasses/classes.cpp
@@ -1,10 +1,24 @@
 
 #include <boost/python.hpp>
+#include <boost/python/list.hpp>
+#include <boost/python/extract.hpp>
 #include <string>
+#include <sstream>
+#include <vector>
 
 struct World
 {
     void set(std::string msg) { mMsg = msg; }
+    void many(boost::python::list msgs) {
+        long l = len(msgs);
+        std::stringstream ss;
+        for (long i = 0; i<l; ++i) {
+            if (i>0) ss << ", ";
+            std::string s = boost::python::extract<std::string>(msgs[i]);
+            ss << s;
+        }
+        mMsg = ss.str();
+    }
     std::string greet() { return mMsg; }
     std::string mMsg;
 };
@@ -16,5 +30,6 @@ BOOST_PYTHON_MODULE(classes)
     class_<World>("World")
         .def("greet", &World::greet)
         .def("set", &World::set)
+        .def("many", &World::many)
     ;
 };
--- 02-ExposingClasses/classes.py
+++ 02-ExposingClasses/classes.py
@@ -6,4 +6,7 @@ t = classes.World()
 t.set("bom dia!")
 print (t.greet())
 
+t.many(['Good Morning', 'Buon giorno', 'Kali mera'])
+print (t.greet())
+
 
